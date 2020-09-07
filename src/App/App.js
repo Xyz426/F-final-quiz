d0cf832a558160b0f4aec33e23299cb2edbd2dc1
xyz <xyzzhybz@163.com>
2020-09-07 16:52:52 +0800

    [xyz]完成header布局

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Header from '../components/header/Header'
 
 class App extends Component {
+
   render() {
     return (
       <div data-testid="app" className="App">
-        Hello World
+        <Header/>
       </div>
     );
   }
