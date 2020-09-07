8b5b5f2585df44fc61ce4e64bc97f45518df294b
xyz <xyzzhybz@163.com>
2020-09-07 20:25:48 +0800

    [xyz]重新完成整体页面分层

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Header from '../components/header/Header'
 
 class App extends Component {
-
+  constructor(props){
+    super(props)
+    this.state={
+        list:[],
+        randomList:[]   
+    }
+}
   render() {
     return (
       <div data-testid="app" className="App">
-        <Header/>
+        <header>
+          <div>
+            <h1 className="title">分组列表</h1>
+            <button className="grouping">分组学员</button>
+          </div>
+        </header>
+
+        <main>
+            {this.state.randomList.map((item,index)=>{
+                        return <li key={index}>{item.id}{item.name}</li>
+                    })
+                  }
+        </main>
+
+        <footer>
+            {this.state.list.map((item,index)=>{
+                        return <li key={index}>{item.id}{item.name}</li>
+                    })
+                  }
+        </footer>
       </div>
     );
   }
