96bee650ac8c28a8a945b148b00c8113b7ca828d
xyz <xyzzhybz@163.com>
2020-09-24 22:00:45 +0800

    [xyz]完成header

--- src/App/App.js
+++ src/App/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './App.scss';
 import TraineeList from '../components/TraineeList/TraineeList';
 import TrainerList from '../components/TrainerList/TrainerList';
-
+import GroupList from '../components/GroupList/GroupList';
 class App extends Component {
   render(){
     return(
       <div className='App'>
+        <GroupList/>
         <TrainerList/>
         <TraineeList/>
       </div>
---
+++ src/components/GroupList/GroupList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './GroupList.scss';
+class GroupList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return (
+            <div className='group-list'>
+                <div className='header-title'>分组列表</div>
+                <button className='group-button'>分组学员</button>
+            </div>
+        )
+    }
+}
+
+export default GroupList;
