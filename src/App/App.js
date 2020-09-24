ae4955af42a7203ea604523dbe934e4b5cc7e56a
xyz <xyzzhybz@163.com>
2020-09-24 21:46:19 +0800

    [xyz] 完成讲师列表展示

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
 import TraineeList from '../components/TraineeList/TraineeList';
-
+import TrainerList from '../components/TrainerList/TrainerList';
 
 class App extends Component {
   render(){
     return(
-      <div className='App'>        
+      <div className='App'>
+        <TrainerList/>
         <TraineeList/>
       </div>
     )
---
+++ src/components/TrainerList/TrainerList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './TrainerList.scss';
+
+class TrainerList extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            trainers:[]
+        }
+    }    
+
+    componentDidMount(){
+        this.getTrainers();
+    }
+
+    getTrainers = () => {
+        fetch('http://localhost:8080/trainers?grouped=false', {
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => {
+            if (response.ok) {
+              return Promise.resolve(response.json());
+            }
+            return Promise.reject();
+          })
+          .then((data) => {
+            this.setState({
+                trainers: data,
+            });
+          })
+          .catch(() => {
+            console.log('err');
+          });
+      };
+    
+        
+    render(){
+        return(
+            <div className='trainer-list'>
+                <div className='header-title'>讲师列表</div>
+                {this.state.trainers.map((item,index) => {
+                    return(
+                        <span key={index} className='trainer'>
+                            {item.id}{item.name}
+                        </span>
+                    )
+                })}       
+            </div>
+        )
+    }
+}
+
+export default TrainerList;
