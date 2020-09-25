807b11a5259f29519910d25921ba8ca0dfb9abdd
mengqiu PENG <mengqiu.peng@thoughtworks.com>
2020-09-25 21:52:11 +0800

    [mengqiu] add feedback

--- src/components/Group/Group.js
+++ src/components/Group/Group.js
@@ -1,38 +1,33 @@
 import React, { Component } from 'react';
 import './Group.scss';
-class Group extends Component{
-    constructor(props){
-        super(props);
-    }
 
-    render(){
-        console.log(this.props.trainers[0].name);
-        return(
-            <div>
-                <header className='group-header'>
-                    <span className='group-name'>
-                        {this.props.name}
-                    </span>  
-                    {this.props.trainers.map((item,index) => {
-                        return (
-                            <span className='group-trainers' key={index}>
-                                {item.id}.{item.name}
-                            </span>
-                        )
-                    })}
-                </header>
-                <main>
-                    {this.props.trainees.map((item,index) => {
-                        return (
-                            <span className='group-trainees' key={index}>
-                                {item.id}.{item.name}
-                            </span>
-                        )
-                    })}  
-                </main>
-            </div>
-        )
-    }
+class Group extends Component {
+  render() {
+    console.log(this.props.trainers[0].name);
+    return (
+      <div>
+        <header className="group-header">
+          <span className="group-name">{this.props.name}</span>
+          {this.props.trainers.map((item, index) => {
+            return (
+              <span className="group-trainers" key={index}>
+                {item.id}.{item.name}
+              </span>
+            );
+          })}
+        </header>
+        <main>
+          {this.props.trainees.map((item, index) => {
+            return (
+              <span className="group-trainees" key={index}>
+                {item.id}.{item.name}
+              </span>
+            );
+          })}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default Group;
--- src/components/GroupList/GroupList.js
+++ src/components/GroupList/GroupList.js
@@ -1,84 +1,88 @@
 import React, { Component } from 'react';
 import './GroupList.scss';
-import Group from '../Group/Group'
+import Group from '../Group/Group';
+
 class GroupList extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            groups: []
-        }
-    }
-
-    componentDidMount() {
-        this.getGroupList();
-    }
-
-    getGroupList = () => {
-        fetch('http://localhost:8080/groups', {
-          method: 'GET',
-          mode: 'cors',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => {
-            if (response.ok) {
-              return Promise.resolve(response.json());
-            }
-            return Promise.reject();
-          })
-          .then((data) => {
-            this.setState({
-                groups: data,
-            });
-          })
-          .catch(() => {
-            console.log('err');
-          });
-      };
-
-    postAutoGrouping = () => {
-      fetch('http://localhost:8080/groups/auto-grouping', {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      })
-        .then((response) => {
-          if (response.ok) {
-            return Promise.resolve(response.json());
-          }
-          return Promise.reject();
-        })
-        .then((data) => {
-          this.setState({
-              groups: data,
-          });
-        })
-        .catch(() => {
-          console.log('err');
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: [],
     };
+  }
 
-    render() {
-        console.log(this.state.groups);
-        return (
-           <div className='groupList'>
-                <header className='header-groupList'>
-                    <div className='header-title'>分组列表</div>
-                    <button className='group-button' onClick={this.postAutoGrouping}>分组学员</button>
-                </header>
-                {this.state.groups.map((item,index) => {
-                    return(
-                        <span key={index}>
-                            <Group name={item.name} trainers={item.trainers} trainees={item.trainees}/>
-                        </span>
-                    )
-                })}       
-            </div>
-        )
-    }
+  componentDidMount() {
+    this.getGroupList();
+  }
+
+  getGroupList = () => {
+    fetch('http://localhost:8080/groups', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return Promise.resolve(response.json());
+        }
+        return Promise.reject();
+      })
+      .then((data) => {
+        this.setState({
+          groups: data,
+        });
+      })
+      .catch(() => {
+        console.log('err');
+      });
+  };
+
+  postAutoGrouping = () => {
+    fetch('http://localhost:8080/groups/auto-grouping', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return Promise.resolve(response.json());
+        }
+        return Promise.reject();
+      })
+      .then((data) => {
+        this.setState({
+          groups: data,
+        });
+      })
+      .catch(() => {
+        console.log('err');
+      });
+  };
+
+  render() {
+    // TODO feedback: 没有删除console
+    console.log(this.state.groups);
+    return (
+      <div className="groupList">
+        <header className="header-groupList">
+          <div className="header-title">分组列表</div>
+          <button className="group-button" onClick={this.postAutoGrouping} type="button">
+            分组学员
+          </button>
+        </header>
+        {this.state.groups.map((item, index) => {
+          return (
+            <span key={index}>
+              <Group name={item.name} trainers={item.trainers} trainees={item.trainees} />
+            </span>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default GroupList;
